e11529921951de56543eb28018c5d03e249130e7
Kevin Tanner <kevindavidtanner@gmail.com>
2019-09-04 14:14:44 -0600

    makes player conform to equatable and removes game and leaderboard models

--- MultiplicationTables/Models/Player.swift
+++ MultiplicationTables/Models/Player.swift
@@ -17,3 +17,9 @@ class Player {
         self.highScore = highScore
     }
 }
+
+extension Player: Equatable {
+    static func == (lhs: Player, rhs: Player) -> Bool {
+        return lhs.name == rhs.name
+    }
+}
